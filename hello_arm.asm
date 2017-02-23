67e2fec771dbe18de3e56b5c046d4b83da1d4982
Marius Podwyszynski <marius.pod@googlemail.com>
2017-02-23 07:26:07 +0100

    Adding the asm file

---
+++ hello_arm.asm
@@ -0,0 +1,17 @@
+format ELF executable 
+        entry start 
+        segment readable executable 
+
+start:  mov     r0,1             ; stdout
+        add     r1,pc,hello-$-8  ; the string
+        mov     r2,hello_len     ; the length of the string
+        mov     r7,4             ; write
+        swi     0                ; invoke
+        mov     r7,1             ; exit
+        mov     r0,0             ; set exit_code
+        swi     0                ; invoke
+
+hello:  db      'Hello Docker World!', 0x0A, 20
+hello_len=$-hello 
+
+        segment writeable
